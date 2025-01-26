bdc3e473549964e6a377eda25473afdeeb67f44c
SoshiNaito <pineappleyogurt1234@gmail.com>
2025-01-27 00:11:04 +0900

    ボタン修正

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,13 @@ export default function Home() {
       <Image src="/noto-senmaida.jpg" alt="能登の千枚田" layout="fill" objectFit="cover" quality={100} />
       <div className="absolute inset-0 bg-black opacity-50" />
       <div className="relative z-10 text-center text-white">
-        <h1 className="text-4xl font-bold mb-8">能登半島ビジネスマッチング</h1>
+        <h1 className="text-6xl font-bold mb-8">All about 能登復興</h1>
+        <h2 className="text-4xl mb-8">〜 能登復興メディア 〜</h2>
         <div className="space-x-4">
-          <Button asChild>
+          <Button asChild variant="outline" className="bg-blue-400">
             <Link href="/articles?category=start">能登でビジネスを始める</Link>
           </Button>
-          <Button asChild variant="outline">
+          <Button asChild variant="outline" className="bg-green-500">
             <Link href="/articles?category=know">能登のビジネスを知る</Link>
           </Button>
         </div>
@@ -21,4 +22,3 @@ export default function Home() {
     </div>
   )
 }
-
